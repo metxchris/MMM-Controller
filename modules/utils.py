3c7aa19d23926597d7d6b755001457e246097dd2
MetxChris <MetxChris@gmail.com>
2021-12-30 14:12:00 -0500

    Updated readme file

--- modules/utils.py
+++ modules/utils.py
@@ -145,7 +145,7 @@ def get_scan_num(runid):
     * ValueError: If there are too many scan number directories
     '''
 
-    num_range = range(100, 10000)
+    num_range = range(1, 10000)
 
     for scan_num in num_range:
         scan_num_path = get_scan_num_path(runid, scan_num)
